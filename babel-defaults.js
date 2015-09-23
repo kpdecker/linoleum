3eff727504ed964730308cdf22cf412da4c9346d
kpdecker <kpdecker@gmail.com>
2015-09-23 18:38:23 -0500

    Properly handle transpile of our own code

--- babel-defaults.js
+++ babel-defaults.js
@@ -1,4 +1,12 @@
+/* eslint-disable object-shorthand */
 module.exports = {
+  // We need to make sure that the transpiler hit the linoleum source since we are
+  // not building to npm or simliar.
+  ignore: function(filename) {
+    return (/node_modules/.test(filename))
+        && !(/linoleum\/src/.test(filename))
+        && !(/linoleum\/tasks/.test(filename));
+  },
   sourceMap: 'inline',
   auxiliaryCommentBefore: 'istanbul ignore next'
 };
