97569d86eecd7a327684746b62c9cbc637857d62
kpdecker <kpdecker@gmail.com>
2015-10-01 05:34:16 -0500

    Debounce watch operations

--- src/watch.js
+++ src/watch.js
@@ -1,11 +1,15 @@
 import Gulp from 'gulp';
+import batch from 'gulp-batch';
+import runSequence from 'run-sequence';
 
 import * as Index from '../index';
 
 export default function(files, command) {
   Gulp.task(`watch:${command}`, function() {
     Index.WATCHING = true;    // Enable plumber
-    Gulp.watch(files, [command]);
+    Gulp.watch(files, batch(function(events, done) {
+      runSequence(command, done);
+    }));
     Gulp.start(command);
   });
 }
