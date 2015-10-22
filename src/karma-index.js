ff4cac5f44a1adabbe7521608ae769d3fc3cfe94
kpdecker <kpdecker@gmail.com>
2015-10-22 14:53:11 -0500

    Fix karma compile fail when installed

--- src/karma-index.js
+++ src/karma-index.js
@@ -1,5 +1,7 @@
+// Using es5 in this module as it won't be transpiled in child projects
+// due to the node_modules babel loader exclude.
 /* istanbul ignore next */
-import './sandbox';
+require('./sandbox');
 
 // require all modules ending in "_test" from the
 // current directory and all subdirectories
