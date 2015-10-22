28b17767adf033a24fc4d5800afc31669d093d31
kpdecker <kpdecker@gmail.com>
2015-10-22 15:00:41 -0500

    Revert "Fix karma compile fail when installed"
    This reverts commit ff4cac5f44a1adabbe7521608ae769d3fc3cfe94.

--- src/karma-index.js
+++ src/karma-index.js
@@ -1,7 +1,5 @@
-// Using es5 in this module as it won't be transpiled in child projects
-// due to the node_modules babel loader exclude.
 /* istanbul ignore next */
-require('./sandbox');
+import './sandbox';
 
 // require all modules ending in "_test" from the
 // current directory and all subdirectories
