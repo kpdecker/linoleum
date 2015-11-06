dcb68704f20d82017d2e0bf26a50f5f3b3d15d68
kpdecker <kpdecker@gmail.com>
2015-11-05 23:31:25 -0600

    Fix babel transpile for electron runner

--- babel-defaults.js
+++ babel-defaults.js
@@ -4,6 +4,7 @@ module.exports = {
   // not building to npm or simliar.
   ignore: function(filename) {
     return ((/node_modules/.test(filename))
+        && !(/linoleum\/electron/.test(filename))
         && !(/linoleum\/src/.test(filename))
         && !(/linoleum\/tasks/.test(filename)))
         || (/\$.*\$/.test(filename));
