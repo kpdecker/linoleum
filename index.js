f6b39ff8bd9737030f072e2f85ae6def820d9b94
kpdecker <kpdecker@gmail.com>
2015-12-02 20:35:46 -0800

    Ensure that there is a singleton for state

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ module.exports.testFiles = function() {
   return module.exports.TEST_FILES;
 };
 
-require('./tasks/clean');
-require('./tasks/lint');
-require('./tasks/cover');
+// Hack around dependency tree issues when trying to self-test with linoleum-node.
+if (global.__linoleum) {
+  module.exports = global.__linoleum;
+} else {
+  require('./tasks/clean');
+  require('./tasks/lint');
+  require('./tasks/cover');
+
+  global.__linoleum = module.exports;
+}
