ee79c43a3a9571266a5a7aa014cef3d42a22b79c
kpdecker <kpdecker@gmail.com>
2015-12-16 07:33:37 -0600

    Move config values into gulp-isolated module

---
+++ config.js
@@ -0,0 +1,27 @@
+// Config values. May be overriden prior to tasks executing.
+module.exports.WATCHING = false;
+
+module.exports.SOURCE_FILES = ['src/**/*.{js,jsx}'];
+module.exports.TEST_FILES = [__dirname + '/src/sandbox.js', 'test/**/*.js'];    // eslint-disable-line prefer-template
+
+module.exports.BUILD_TARGET = 'lib/';
+module.exports.COVERAGE_TARGET = './coverage';
+
+module.exports.COMPLETE_COVERAGE = true;
+
+// Hack around es6 module definitions without bootstrapping the transpiler
+module.exports.__esModule = true;
+
+require('./runtime-init');
+
+module.exports.watch = require('./src/watch').default;
+module.exports.runTask = require('./src/watch').runTask;
+
+module.exports.jsFiles = function() {
+  return module.exports.SOURCE_FILES.concat(
+      module.exports.TEST_FILES);
+};
+
+module.exports.testFiles = function() {
+  return module.exports.TEST_FILES;
+};
--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-// Config values. May be overriden prior to tasks executing.
-module.exports.WATCHING = false;
-
-module.exports.SOURCE_FILES = ['src/**/*.{js,jsx}'];
-module.exports.TEST_FILES = [__dirname + '/src/sandbox.js', 'test/**/*.js'];    // eslint-disable-line prefer-template
-
-module.exports.BUILD_TARGET = 'lib/';
-module.exports.COVERAGE_TARGET = './coverage';
-
-module.exports.COMPLETE_COVERAGE = true;
-
-// Hack around es6 module definitions without bootstrapping the transpiler
-module.exports.__esModule = true;
-
-require('./runtime-init');
-
-module.exports.watch = require('./src/watch').default;
-module.exports.runTask = require('./src/watch').runTask;
-
-module.exports.jsFiles = function() {
-  return module.exports.SOURCE_FILES.concat(
-      module.exports.TEST_FILES);
-};
-
-module.exports.testFiles = function() {
-  return module.exports.TEST_FILES;
-};
+module.exports = require('./config');
 
 // Hack around dependency tree issues when trying to self-test with linoleum-node.
 if (global.__linoleum) {
