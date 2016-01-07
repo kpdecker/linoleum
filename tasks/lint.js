2bcbf9cbc40b648f389b7a0c250dac60eba07f20
kpdecker <kpdecker@gmail.com>
2016-01-07 14:04:27 -0600

    Only lint changed files in watch mode

--- tasks/lint.js
+++ tasks/lint.js
@@ -1,12 +1,32 @@
 import Gulp from 'gulp';
+import changed from 'gulp-changed';
 import eslint from 'gulp-eslint';
 import plumber from '../src/plumber';
 
-import {jsFiles} from '../index';
+import {jsFiles, BUILD_TARGET} from '../index';
+
+import {changeMonitors} from '../src/watch';
+
+let fileCache = {};
+changeMonitors.on('change', (changed) => {
+  changed.forEach((change) => {
+    delete fileCache[change.path];
+  });
+});
 
 Gulp.task('lint', function() {
   return Gulp.src(jsFiles())
       .pipe(plumber())
+      .pipe(changed(BUILD_TARGET, {
+        hasChanged(stream, callback, sourceFile) {
+          let modified = fileCache[sourceFile.path];
+          if (!modified || modified < sourceFile.stat.mtime) {
+            stream.push(sourceFile);
+            fileCache[sourceFile.path] = sourceFile.stat.mtime;
+          }
+          callback();
+        }
+      }))
       .pipe(eslint())
       .pipe(eslint.format())
       .pipe(eslint.failAfterError());
